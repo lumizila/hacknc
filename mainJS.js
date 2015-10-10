95ac287be25f6805b6db8e1ae51480603d9d55a6
Christian <cbcook321@gmail.com>
2015-10-10 18:36:32 -0400

    UPDATES

--- mainJS.js
+++ mainJS.js
@@ -3,15 +3,30 @@ var animalList = [{name:"pig", id: 1}, {name:"cow", id: 2}, {name:"dog", id: 3},
 var count = 0;
 var attempts = 0;
 var score =0;
-var cell1, cell2, cell3, cell4;
+var animalList = [{name:"pig", id: 1}, {name:"cow", id: 2}, {name:"dog", id: 3}, {name:"cat", id:4}, {name:"duck", id: 5}, {name:"lion", id: 6}, {name: "monkey", id: 7} , {name:"chicken", id: 8}, {name:"elephant", id: 9}, {name:"bird", id:10}];
+
+var count = 0;
+var attempts = 0;
+var score =0;
+var cells = [];
 $(document).ready(function () {
-    $(document).on('click', 'td', function (e){
-        var optionsList = collectOptions();
-        for(i = 0; i < optionsList.length; i ++){
-            console.log(optionsList[i]);   
+    $("#next").on('click', function (e){
+        var id =  Math.floor(Math.random() * (10 - 0) + 0);
+        if(count === 4){
+            
+        }else{
+            var optionsList = collectOptions();
+            var cell1, cell2, cell3, cell4;
+            cells.push(document.getElementById('1'));
+            cells.push(document.getElementById('2'));
+            cells.push(document.getElementById('3'));
+            cells.push(document.getElementById('4'));
+            for(i = 0; i < optionsList.length; i++){
+                var identifier = cells[i].getAttribute('src');
+            }
         }
+        count = count + 1;
     });
-});
 
 
 function collectOptions(){
