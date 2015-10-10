e9c4f866d2d8629e8fbcdc47f1375500f0e23cbf
LeroyAnd3 <leroyanderson@triad.rr.com>
2015-10-10 14:56:59 -0400

    Changed file type

---
+++ app.css
@@ -0,0 +1,22 @@
+html#main {
+    background-color: #0971CF;
+    <!--#2680D0-->
+    <!--#4D94D4-->
+}
+html#quiz {
+    background-color: #FF1800;
+    <!--#E91600-->
+    <!--#B51100-->
+}
+
+html#popup {
+    background-color: #00E400;
+    <!--#00BA00-->
+    <!--#009100-->
+}
+
+html#score {
+    background-color: #FFF400;
+    <!--#B5AD00-->
+    <!--#E9DD00-->
+}
