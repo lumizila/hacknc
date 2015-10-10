0bf255159f56b9f764674a474422ff881f82b55c
Christian <cbcook321@gmail.com>
2015-10-10 18:23:09 -0400

    Added functionality

--- mainJS.js
+++ mainJS.js
@@ -11,6 +11,54 @@ $(document).ready(function () {
             console.log(optionsList[i]);   
         }
     });
+    $("img#1").on('click', function() {
+	
+		var aux = document.getElementById('1').getAttribute('src');
+		var aux1 = document.getElementById('sound').getAttribute('src');
+		aux1 = aux1.substr(7);
+		var res = aux1.split(".");
+		aux1 = res[0];
+		if(aux.indexOf(aux1) > -1 ){ // correct
+			alert("clicked on image 1, its correct");
+		}
+	});
+	
+	$("img#2").on('click', function() {
+	
+		var aux = document.getElementById('2').getAttribute('src');
+		var aux1 = document.getElementById('sound').getAttribute('src');
+		aux1 = aux1.substr(7);
+		var res = aux1.split(".");
+		aux1 = res[0];
+		if(aux.indexOf(aux1) > -1 ){ // correct
+			alert("wow");
+		}
+	});
+	
+	$("img#3").on('click', function() {
+	
+		var aux = document.getElementById('3').getAttribute('src');
+		var aux1 = document.getElementById('sound').getAttribute('src');
+		aux1 = aux1.substr(7);
+		var res = aux1.split(".");
+		aux1 = res[0];
+
+		if(aux.indexOf(aux1) > -1 ){ // correct
+			alert("wow");
+		}
+	});
+	
+	$("img#4").on('click', function() {
+	
+		var aux = document.getElementById('4').getAttribute('src');
+		var aux1 = document.getElementById('sound').getAttribute('src');
+		aux1 = aux1.substr(7);
+		var res = aux1.split(".");
+		aux1 = res[0];
+		if(aux.indexOf(aux1) > -1 ){ //correct
+			alert("wow");
+		}
+	});
 });
 
 
