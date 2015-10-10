f304cbb82801f8cd343efe72f5da36ba1084c3a1
cookcb <cbcook321@gmail.com>
2015-10-10 16:59:33 -0400

    Update mainJS.js

--- mainJS.js
+++ mainJS.js
@@ -1,1 +1,36 @@
+var animalList = [{name:"pig", id: 1}, {name:"cow", id: 2}, {name:"dog", id: 3}, {name:"cat", id:4}, {name:"duck", id: 5}, {name:"lion", id: 6}, {name: "monkey", id: 7} , {name:"chicken", id: 8}, {name:"elephant", id: 9}, {name:"bird", id:10}];
 
+
+
+function collectOptions(){
+    var options = new Array();
+    var ids = [];
+    
+    while(ids.length < 4){
+        var id =  Math.floor(Math.random() * (10 - 0) + 0);
+        var duplicate = false;
+        for(i = 0; i < ids.length; i++){
+            if(ids[i] == id){
+                duplicate = true;
+                break;
+            }
+        }
+        if(duplicate === false){
+            //console.log(id);
+            ids[ids.length] = id;   
+            
+        }
+    }
+    for(i = 0; i < 4; i++){
+        console.log(ids[i]);
+        options.push(animalList[ids[i]].name);   
+    }
+    return options;
+}
+function popUp(inputAnswer){
+    if(answer === "correct"){
+        alert("CORRECT, GREAT JOB");   
+    }else{
+        alert("OOPS, TRY AGAIN");  
+    }
+}
