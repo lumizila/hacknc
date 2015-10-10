1ee2243cdfeb59d9b31f9e2664047c89a67f4649
Christian <cbcook321@gmail.com>
2015-10-10 17:31:36 -0400

    Added ID's and variables in JS

--- mainJS.js
+++ mainJS.js
@@ -2,7 +2,12 @@ var animalList = [{name:"pig", id: 1}, {name:"cow", id: 2}, {name:"dog", id: 3},
 var count = 0;
 var attempts = 0;
 var score =0;
+var cell1, cell2, cell3, cell4;
 
+cell1 = document.getElementById("one");
+cell2 = document.getElementById("two");
+cell3 = document.getElementById("three");
+cell4 = document.getElementById("four");
 function collectOptions(){
     var options = new Array();
     var ids = [];
